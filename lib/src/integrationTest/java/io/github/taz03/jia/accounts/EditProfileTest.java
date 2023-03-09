89c95776fda66a0aeac6e4560b82d224c0563bee
Taz03 <tanishazad03@gmail.com>
2023-03-09 17:24:42 +0530

    added edit profile request

---
+++ lib/src/integrationTest/java/io/github/taz03/jia/accounts/EditProfileTest.java
@@ -0,0 +1,10 @@
+package io.github.taz03.jia.accounts;
+
+import org.junit.jupiter.api.Test;
+
+public final class EditProfileTest {
+    @Test
+    public void editProfileTest() throws Exception {
+        // TODO: write integration test
+    }
+}
