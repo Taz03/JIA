23865b11c16e22acce1ee8a6e73486cbdd4d3944
Taz03 <tanishazad03@gmail.com>
2023-03-11 04:22:25 +0530

    integration test

--- lib/src/integrationTest/java/io/github/taz03/jia/accounts/EditProfileTest.java
+++ lib/src/integrationTest/java/io/github/taz03/jia/accounts/EditProfileTest.java
@@ -1,10 +1,19 @@
 package io.github.taz03.jia.accounts;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static io.github.taz03.jia.TestClient.*;
+
 import org.junit.jupiter.api.Test;
+
+import io.github.taz03.jia.requests.accounts.EditProfileRequest;
+import io.github.taz03.jia.responses.accounts.EditProfileResponse;
 
 public final class EditProfileTest {
     @Test
     public void editProfileTest() throws Exception {
-        // TODO: write integration test
+        EditProfileRequest request = new EditProfileRequest("JIA", "JIA test bio [edit profile request]", user.username(), user.phoneNumber(), user.email(), "");
+        EditProfileResponse response = client.sendRequest(request).get();
+
+        assertEquals("ok", response.getBaseResponse().getStatus());
     }
 }
--- lib/src/integrationTest/java/io/github/taz03/jia/accounts/SetBiographyTest.java
+++ lib/src/integrationTest/java/io/github/taz03/jia/accounts/SetBiographyTest.java
@@ -1,10 +1,10 @@
 package io.github.taz03.jia.accounts;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static io.github.taz03.jia.TestClient.*;
 
 import org.junit.jupiter.api.Test;
 
-import io.github.taz03.jia.TestClient;
 import io.github.taz03.jia.requests.accounts.SetBiographyRequest;
 import io.github.taz03.jia.responses.InstagramBaseResponse;
 
@@ -12,7 +12,7 @@ public final class SetBiographyTest {
     @Test
     public void setBioTest() throws Exception {
         SetBiographyRequest request = new SetBiographyRequest("Test bio from JIA library!");
-        InstagramBaseResponse response = TestClient.client.sendRequest(request).get();
+        InstagramBaseResponse response = client.sendRequest(request).get();
 
         assertEquals("ok", response.getStatus());
     }
