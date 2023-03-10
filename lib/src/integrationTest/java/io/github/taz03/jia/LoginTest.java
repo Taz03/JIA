71462c26073dfe196aac2a80ceaa862a669543a6
Taz03 <tanishazad03@gmail.com>
2023-03-10 15:48:37 +0530

    used json for user credentials

--- lib/src/integrationTest/java/io/github/taz03/jia/LoginTest.java
+++ lib/src/integrationTest/java/io/github/taz03/jia/LoginTest.java
@@ -2,18 +2,22 @@ package io.github.taz03.jia;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvFileSource;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import org.junit.jupiter.api.Test;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public final class LoginTest {
-    @ParameterizedTest
-    @CsvFileSource(resources = "/user.csv")
-    public void loginTest(String username, String password) {
-        InstagramClient client = new InstagramClient(username, password);
+    @Test
+    public void loginTest() throws Exception {
+        TestClient.user = new ObjectMapper().readValue(Files.readString(Path.of("lib/src/integrationTest/resources/user.json")), TestClient.User.class);
+
+        InstagramClient client = new InstagramClient(TestClient.user.username(), TestClient.user.password());
         client.login();
 
         assertNotNull(client.getAuthorization());
-
-        TestClient.setClient(client);
+        TestClient.client = client;
     }
 }
