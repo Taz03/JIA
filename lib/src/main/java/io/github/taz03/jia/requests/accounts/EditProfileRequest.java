e53a143d5cd919a48c6e3320c21bccf4663c61dd
Tanish <73871477+Taz03@users.noreply.github.com>
2023-03-11 04:29:27 +0530

    added edit profile request (#9)

---
+++ lib/src/main/java/io/github/taz03/jia/requests/accounts/EditProfileRequest.java
@@ -0,0 +1,38 @@
+package io.github.taz03.jia.requests.accounts;
+
+import java.util.Map;
+
+import io.github.taz03.jia.requests.InstagramPostRequest;
+import io.github.taz03.jia.responses.accounts.EditProfileResponse;
+
+/**
+ * Represents an Instagram edit profile request, used to edit profile data such as username, phone number, etc.
+ * <br><br>
+ * Endpoint: <i>/accounts/edit_profile/</i>
+ * <br>
+ * Method: POST
+ * <br>
+ * Response: {@link EditProfileResponse}
+ */
+public final class EditProfileRequest extends InstagramPostRequest<EditProfileResponse> {
+	/**
+     * Creates an Instagram edit profile request.
+     *
+	 * @param name        New display name to set
+	 * @param biography   New biography to set
+	 * @param username    New username to set
+	 * @param phoneNumber New phone numer to set
+	 * @param email       New email to set
+	 * @param externalUrl New external url to set
+	 */
+	public EditProfileRequest(String name, String biography, String username, String phoneNumber, String email, String externalUrl) {
+		super(EditProfileResponse.class, "/accounts/edit_profile/", null, Map.of(
+            "first_name", name,
+            "biography", biography,
+            "username", username,
+            "phone_number", phoneNumber,
+            "email", email,
+            "external_url", externalUrl
+        ));
+	}
+}
