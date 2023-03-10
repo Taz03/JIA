1a80fde67441cee9b2799b241e9b9c6fe98af60b
Tanish <73871477+Taz03@users.noreply.github.com>
2023-03-10 15:59:43 +0530

    used json for user credentials (#10)

--- lib/src/integrationTest/java/io/github/taz03/jia/TestClient.java
+++ lib/src/integrationTest/java/io/github/taz03/jia/TestClient.java
@@ -1,14 +1,22 @@
 package io.github.taz03.jia;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 // Proxy Client
 public final class TestClient {
-    private static InstagramClient client;
+    public static InstagramClient client;
+    public static User user;
 
-    public static InstagramClient getClient() {
-        return client;
-    }
-
-    public static void setClient(InstagramClient client) {
-        TestClient.client = client;
-    }
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static final record User(
+        @JsonProperty("username")
+        String username,
+        @JsonProperty("password")
+        String password,
+        @JsonProperty("phone_number")
+        String phoneNumber,
+        @JsonProperty("email")
+        String email
+    ) {}
 }
