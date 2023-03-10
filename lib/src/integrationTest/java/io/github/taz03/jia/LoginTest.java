97c1b6e78ea495ebf89558720ae882988cbb52b0
Taz03 <tanishazad03@gmail.com>
2023-03-11 05:12:02 +0530

    separated user initilization

--- lib/src/integrationTest/java/io/github/taz03/jia/LoginTest.java
+++ lib/src/integrationTest/java/io/github/taz03/jia/LoginTest.java
@@ -5,15 +5,19 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public final class LoginTest {
-    @Test
-    public void loginTest() throws Exception {
+    @BeforeAll
+    public static void initTestClientUser() throws Exception {
         TestClient.user = new ObjectMapper().readValue(Files.readString(Path.of("src/integrationTest/resources/user.json")), TestClient.User.class);
+    }
 
+    @Test
+    public void loginTest() {
         InstagramClient client = new InstagramClient(TestClient.user.username(), TestClient.user.password());
         client.login();
 
