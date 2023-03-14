3db2eefbb0439e366288a43412fa1bf32a1bcb7f
Taz03 <tanishazad03@gmail.com>
2023-03-14 21:30:32 +0530

    added request

---
+++ lib/src/integrationTest/java/io/github/taz03/jia/accounts/SetVisibilityTest.java
@@ -0,0 +1,28 @@
+package io.github.taz03.jia.accounts;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static io.github.taz03.jia.TestClient.*;
+
+import java.util.List;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import io.github.taz03.jia.requests.accounts.SetVisibilityRequest;
+import io.github.taz03.jia.requests.accounts.SetVisibilityRequest.Visiblity;
+import io.github.taz03.jia.responses.accounts.UserResponse;
+
+public final class SetVisibilityTest {
+    @ParameterizedTest
+    @MethodSource("getVisiblities")
+    public void setVisibilityTest(Visiblity visiblity) throws Exception {
+        SetVisibilityRequest request = new SetVisibilityRequest(visiblity);
+        UserResponse response = client.sendRequest(request).get();
+
+        assertEquals("ok", response.getBaseResponse().getStatus());
+    }
+    
+    private static List<Visiblity> getVisiblities() {
+        return List.of(Visiblity.PUBLIC, Visiblity.PRIVATE);
+    }
+}
---
+++ lib/src/main/java/io/github/taz03/jia/requests/accounts/SetVisibilityRequest.java
@@ -0,0 +1,35 @@
+package io.github.taz03.jia.requests.accounts;
+
+import io.github.taz03.jia.requests.InstagramPostRequest;
+import io.github.taz03.jia.responses.accounts.UserResponse;
+
+/**
+ * Represents an Instagram set visiblity request, used to set visiblity of Instagram account(public or private).
+ * <br><br>
+ * Endpoint: <i>/accounts/set_public/</i> OR <i>/accounts/set_private</i>
+ */
+public final class SetVisibilityRequest extends InstagramPostRequest<UserResponse> {
+    public static enum Visiblity {
+        PUBLIC("set_public/"),
+        PRIVATE("set_private/");
+
+        private final String path;
+
+        private Visiblity(String path) {
+            this.path = path;
+        }
+
+        public String getPath() {
+            return path;
+        }
+    }
+
+	/**
+     * Creates an Instagram set visiblity request.
+     *
+	 * @param visiblity The account visiblity to set
+	 */
+	public SetVisibilityRequest(Visiblity visiblity) {
+		super(UserResponse.class, "/accounts/" + visiblity.getPath());
+	}
+}
