f47c0f2b5987ab0e1caccf7f8583a2137f732dcc
Taz03 <tanishazad03@gmail.com>
2023-03-17 16:23:34 +0530

    deleted unused class

--- lib/src/main/java/io/github/taz03/jia/responses/InstagramBaseResponse.java
+++
@@ -1,7 +0,0 @@
-package io.github.taz03.jia.responses;
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-//public final class InstagramBaseResponse implements InstagramResponse {
-//}
