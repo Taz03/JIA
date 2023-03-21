2874330ae67975efd606d148561caad819ac5aff
Taz03 <tanishazad03@gmail.com>
2023-03-21 12:27:40 +0530

    rename

--- lib/src/integrationTest/java/io/github/taz03/jia/accounts/SetBiographyTest.java
+++ lib/src/integrationTest/java/io/github/taz03/jia/accounts/SetBiographyTest.java
@@ -1,7 +1,7 @@
 package io.github.taz03.jia.accounts;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static io.github.taz03.jia.TestClient.*;
+import static io.github.taz03.jia.TestProxy.*;
 
 import org.junit.jupiter.api.Test;
 
