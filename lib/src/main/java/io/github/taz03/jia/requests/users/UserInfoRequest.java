cff90dfec14ca8360e9f352a9f7a22b44dbd10d7
Taz03 <tanishazad03@gmail.com>
2023-03-23 07:39:04 +0530

    added javadocs to user info request

--- lib/src/main/java/io/github/taz03/jia/requests/users/UserInfoRequest.java
+++ lib/src/main/java/io/github/taz03/jia/requests/users/UserInfoRequest.java
@@ -3,7 +3,17 @@ package io.github.taz03.jia.requests.users;
 import io.github.taz03.jia.requests.InstagramGetRequest;
 import io.github.taz03.jia.responses.users.UserResponse;
 
+/**
+ * Represents an Instagram user info request, used to get details about an Instagram user.
+ *
+ * Endpoint: <i>/users/\<pk\>/info
+ */
 public final class UserInfoRequest extends InstagramGetRequest<UserResponse> {
+    /**
+     * Creates an Instagram user info request.
+     *
+     * @param pk pk of the user to get info for
+     */
     public UserInfoRequest(long pk) {
         super(UserResponse.class, "/users/" + pk + "/info/");
     }
