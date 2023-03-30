8fa2d0f7db8696e681b5ee7de869b39e6afeb776
Taz03 <tanishazad03@gmail.com>
2023-03-30 23:07:32 +0530

    javadoc and integration test

--- lib/src/integrationTest/java/io/github/taz03/jia/rupload/RuploadIgphotoTest.java
+++ lib/src/integrationTest/java/io/github/taz03/jia/rupload/RuploadIgphotoTest.java
@@ -3,7 +3,7 @@ package io.github.taz03.jia.rupload;
 import static io.github.taz03.jia.TestProxy.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.io.File;
+import java.nio.file.Path;
 
 import org.junit.jupiter.api.Test;
 
@@ -13,7 +13,7 @@ import io.github.taz03.jia.responses.InstagramResponse;
 public final class RuploadIgphotoTest {
     @Test
     public void ruploadIgphotoTest() throws Exception {
-        RuploadIgphotoRequest request = new RuploadIgphotoRequest(new File("/home/taz/Pictures/Saved Pictures/profile.png"));
+        RuploadIgphotoRequest request = new RuploadIgphotoRequest(Path.of("src/integrationTest/resources/profile.png"));
         InstagramResponse response = client.sendRequest(request).get();
 
         assertEquals("ok", response.getStatus());
--- lib/src/main/java/io/github/taz03/jia/requests/rupload/RuploadIgphotoRequest.java
+++ lib/src/main/java/io/github/taz03/jia/requests/rupload/RuploadIgphotoRequest.java
@@ -1,29 +1,39 @@
 package io.github.taz03.jia.requests.rupload;
 
-import java.io.File;
 import java.io.IOException;
 import java.net.http.HttpRequest.BodyPublishers;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.ThreadLocalRandom;
 
 import io.github.taz03.jia.InstagramClient;
 import io.github.taz03.jia.requests.InstagramPostRequest;
 import io.github.taz03.jia.responses.rupload.RuploadIgphotoResponse;
 
+/**
+ * Represents an Instagram rupload igphoto request, used to get details about an Instagram user.
+ */
 public final class RuploadIgphotoRequest extends InstagramPostRequest<RuploadIgphotoResponse> {
     private final byte[] array;
-    private final String imgType;
 
-    public RuploadIgphotoRequest(File photo) throws IOException {
-        this(Files.readAllBytes(photo.toPath()), photo.getName().split("\\.")[1]);
+    /**
+     * Creates an Instagram rupload igphoto request.
+     *
+     * @param path path to the photo to upload
+     */
+    public RuploadIgphotoRequest(Path path) throws IOException {
+        this(Files.readAllBytes(path));
     }
 
-	public RuploadIgphotoRequest(byte[] array, String imgType) {
+	/**
+     * Creates an Instagram rupload igphoto request.
+     *
+	 * @param array byte array created from the photo to upload
+	 */
+	public RuploadIgphotoRequest(byte[] array) {
 		super(RuploadIgphotoResponse.class, "/rupload_igphoto/" + createName(), null, BodyPublishers.ofByteArray(array));
         this.array = array;
-        this.imgType = imgType;
 	}
 
     @Override
@@ -33,16 +43,15 @@ public final class RuploadIgphotoRequest extends InstagramPostRequest<RuploadIgp
 
         Map<String, Object> headers = new HashMap<>();
         headers.putAll(super.getHeaders(client));
-        headers.put("X-Instagram-Rupload-Params", "{\"upload_id\":\"%s\",\"media_type\":\"1\"}".formatted(name.split("_0_")[0]));
+        headers.put("X-Instagram-Rupload-Params", "{\"upload_id\":\"%s\",\"media_type\":\"1\"}".formatted(name));
         headers.put("X-Entity-Name", name);
         headers.put("X-Entity-Length", array.length);
-        headers.put("X-Entity-Type", "image/" + imgType);
         headers.put("Offset", 0);
 
         return headers;
     }
 
     private static String createName() {
-        return System.currentTimeMillis() + "_0_" + ThreadLocalRandom.current().nextLong(1_000_000_000L, 9_999_999_999L);
+        return String.valueOf(System.currentTimeMillis());
     }
 }
