838c4928a79d76190c4539ac7919b860c8cabdf7
Tanish <tanishazad03@gmail.com>
2023-03-31 21:02:48 +0530

    added change profile request (#23)

---
+++ lib/src/main/java/io/github/taz03/jia/requests/accounts/ChangeProfilePictureRequest.java
@@ -0,0 +1,28 @@
+package io.github.taz03.jia.requests.accounts;
+
+import java.util.Map;
+
+import io.github.taz03.jia.requests.InstagramPostRequest;
+import io.github.taz03.jia.responses.users.UserResponse;
+
+/**
+ * Represents an Instagram change profile picture request, used to change profile.
+ *
+ * To get the upload id, you will have to first upload a photo using {@link io.github.taz03.jia.requests.rupload.RuploadIgphotoRequest}
+ * <br>
+ * Example:
+ * {@snippet :
+ * RuploadIgphotoRequest photoUploadRequest = new RuploadIgphotoRequest(Path.of("/path/to/photo.png"));
+ * RuploadIgphotoResponse photoUploadResponse = client.sendRequest(photoUploadRequest).get();
+ * 
+ * ChangeProfilePictureRequest changeProfileRequest = new ChangeProfilePictureRequest(photoUploadResponse.getUploadId());
+ * }
+ */
+public final class ChangeProfilePictureRequest extends InstagramPostRequest<UserResponse> {
+    /**
+     * Creates an Instagram change profile picture request.
+     */
+	public ChangeProfilePictureRequest(String uploadId) {
+		super(UserResponse.class, "/api/v1/accounts/change_profile_picture/", null, Map.of("upload_id", uploadId));
+	}
+}
