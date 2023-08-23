834663e1c2b8a84638d61f4715d49c91e5c1e4b7
noirtier-villefort <polusash@gmail.com>
2023-08-24 01:05:52 +0300

    Set List type inside FriendshipsFeedResponse

--- lib/src/main/java/io/github/taz03/jia/responses/users/FriendshipsFeedResponse.java
+++ lib/src/main/java/io/github/taz03/jia/responses/users/FriendshipsFeedResponse.java
@@ -3,13 +3,14 @@ package io.github.taz03.jia.responses.users;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.github.taz03.jia.responses.InstagramResponse;
+import io.github.taz03.jia.responses.models.user.User;
 
 import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class FriendshipsFeedResponse extends InstagramResponse {
     @JsonProperty("users")
-    private List users;
+    private List<User> users;
 
     public List getUsers() {
         return users;
