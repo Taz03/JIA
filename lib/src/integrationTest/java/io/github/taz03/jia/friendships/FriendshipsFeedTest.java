250b84310c2dc45985db2c8ee22befeffbabb112
Tanish <tanishazad03@gmail.com>
2023-08-27 13:33:25 +0530

    Update FriendshipsFeedTest.java

--- lib/src/integrationTest/java/io/github/taz03/jia/friendships/FriendshipsFeedTest.java
+++ lib/src/integrationTest/java/io/github/taz03/jia/friendships/FriendshipsFeedTest.java
@@ -5,10 +5,10 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import io.github.taz03.jia.requests.friendships.FriendshipsFeedRequest;
 import io.github.taz03.jia.responses.friendships.FriendshipsFeedResponse;
+
 import org.junit.jupiter.api.Test;
 
 public class FriendshipsFeedTest {
-
     @Test
     public void friendshipsFeedFollowingTest() throws Exception {
         FriendshipsFeedRequest friendshipsFeedFollowing = new FriendshipsFeedRequest(client.getPk(), FriendshipsFeedRequest.FriendshipsFeeds.FOLLOWING);
