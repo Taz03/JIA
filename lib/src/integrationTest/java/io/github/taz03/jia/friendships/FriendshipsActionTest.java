74bd149a7e16df783ecb98fe9a7d3780921367fa
noirtier-villefort <polusash@gmail.com>
2023-09-04 02:56:55 +0300

    FriendshipsActionTest - separate request and response. Follow on instagram official pk FriendshipsActionResponse - Add @JsonIgnoreProperties, @JsonProperty to Status field and rename it

--- lib/src/integrationTest/java/io/github/taz03/jia/friendships/FriendshipsActionTest.java
+++ lib/src/integrationTest/java/io/github/taz03/jia/friendships/FriendshipsActionTest.java
@@ -2,6 +2,7 @@ package io.github.taz03.jia.friendships;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import io.github.taz03.jia.LoginTest;
 import io.github.taz03.jia.TestProxy;
 import io.github.taz03.jia.requests.friendships.FriendshipsActionRequest;
 import io.github.taz03.jia.responses.friendships.FriendshipsActionResponse;
@@ -9,16 +10,20 @@ import org.junit.jupiter.api.Test;
 
 public class FriendshipsActionTest {
     @Test
-    public void unfollowPkTest() throws Exception{
-        long pk = 3333; // your pk here
-        FriendshipsActionResponse response = TestProxy.client.sendRequest(new FriendshipsActionRequest(pk, FriendshipsActionRequest.FriendshipsAction.DESTROY)).get();
-        assertEquals("ok", response.getStatus());
+    public void followUsernameTest() throws Exception{
+        long pk = 25025320;
+        FriendshipsActionRequest friendshipsActionRequest = new FriendshipsActionRequest(pk, FriendshipsActionRequest.FriendshipsAction.CREATE);
+        FriendshipsActionResponse friendshipsActionResponse = TestProxy.client.sendRequest(friendshipsActionRequest).get();
+
+        assertEquals("ok", friendshipsActionResponse.getStatus());
     }
 
     @Test
-    public void followUsernameTest() throws Exception{
-        long pk = 3333; // your pk here
-        FriendshipsActionResponse response = TestProxy.client.sendRequest(new FriendshipsActionRequest(pk, FriendshipsActionRequest.FriendshipsAction.CREATE)).get();
+    public void unfollowPkTest() throws Exception{
+        long pk = 25025320;
+        FriendshipsActionRequest friendshipsActionRequest = new FriendshipsActionRequest(pk, FriendshipsActionRequest.FriendshipsAction.DESTROY);
+        FriendshipsActionResponse response = TestProxy.client.sendRequest(friendshipsActionRequest).get();
+
         assertEquals("ok", response.getStatus());
     }
 }
--- lib/src/main/java/io/github/taz03/jia/responses/friendships/FriendshipsActionResponse.java
+++ lib/src/main/java/io/github/taz03/jia/responses/friendships/FriendshipsActionResponse.java
@@ -3,6 +3,11 @@ package io.github.taz03.jia.responses.friendships;
 import io.github.taz03.jia.responses.InstagramResponse;
 import io.github.taz03.jia.responses.models.friendship.Status;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class FriendshipsActionResponse extends InstagramResponse {
-    private Status friendship_status;
+   @JsonProperty("friendship_status")
+    private Status friendshipStatus;
 }
