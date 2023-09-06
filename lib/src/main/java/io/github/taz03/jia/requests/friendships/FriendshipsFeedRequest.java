b728c86fa705243592a125dfee0fdd8b49f4925d
Tanish <tanishazad03@gmail.com>
2023-09-06 13:06:52 +0530

    Update FriendshipsFeedRequest.java

--- lib/src/main/java/io/github/taz03/jia/requests/friendships/FriendshipsFeedRequest.java
+++ lib/src/main/java/io/github/taz03/jia/requests/friendships/FriendshipsFeedRequest.java
@@ -1,16 +1,18 @@
 package io.github.taz03.jia.requests.friendships;
 
+import java.util.Map;
+
 import io.github.taz03.jia.requests.InstagramGetRequest;
 import io.github.taz03.jia.responses.friendships.FriendshipsFeedResponse;
 
 /**
- * Represents an Instagram friendships feed request, used to get user's followers or followings
+ * Represents an Instagram friendships feed request, used to get followers/followings list
  */
-public final class FriendshipsFeedRequest extends InstagramGetRequest<FriendshipsFeedResponse>   {
+public final class FriendshipsFeedRequest extends InstagramGetRequest<FriendshipsFeedResponse> {
     /**
      * Creates an Instagram friendships feed request
      
-     * @param pk pk of the user to get friendships of
+     * @param pk     pk of the user to get friendships of
      * @param action action of {@link FriendshipsFeeds} to get followers or followings
      */
     public FriendshipsFeedRequest(long pk, FriendshipsFeeds action) {
@@ -20,9 +22,9 @@ public final class FriendshipsFeedRequest extends InstagramGetRequest<Friendship
     /**
      * Creates an Instagram friendships feed request with maxId
 
-     * @param pk pk of the user to get friendships of
+     * @param pk     pk of the user to get friendships of
      * @param action action of {@link FriendshipsFeeds} to get followers or followings
-     * @param maxId value defines from which page the feed should start
+     * @param maxId  beginning number of the followers/followings list
      */
     public FriendshipsFeedRequest(long pk, FriendshipsFeeds action, String maxId) {
         super(FriendshipsFeedResponse.class, String.format("/api/v1/friendships/%d/%s/", pk, action.name().toLowerCase(), Map.of(
@@ -31,7 +33,7 @@ public final class FriendshipsFeedRequest extends InstagramGetRequest<Friendship
     }
 
     /**
-     * Used to form URL in {@linkplain FriendshipsFeedRequest} constructor
+     * Used to form URL in {@link FriendshipsFeedRequest} constructor
      */
     public enum FriendshipsFeeds{
         FOLLOWERS, FOLLOWING
