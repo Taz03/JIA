86d19fa17b089dbd2a6ceca55c9d1764b1984235
Tanish <tanishazad03@gmail.com>
2023-09-06 13:43:52 +0530

    Update FriendshipsFeedTest.java

--- lib/src/integrationTest/java/io/github/taz03/jia/friendships/FriendshipsFeedTest.java
+++ lib/src/integrationTest/java/io/github/taz03/jia/friendships/FriendshipsFeedTest.java
@@ -1,65 +1,27 @@
 package io.github.taz03.jia.friendships;
 
 import static io.github.taz03.jia.TestProxy.client;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import io.github.taz03.jia.requests.friendships.FriendshipsFeedRequest;
 import io.github.taz03.jia.responses.friendships.FriendshipsFeedResponse;
 
-import io.github.taz03.jia.responses.models.user.User;
 import org.junit.jupiter.api.Test;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class FriendshipsFeedTest {
     @Test
     public void friendshipsFeedFollowingTest() throws Exception {
-        String maxId;
-        List<User> followersList = new ArrayList<>();
+        FriendshipsFeedRequest followingsRequest = new FriendshipsFeedRequest(client.getPk(), FriendshipsFeedRequest.FriendshipsFeeds.FOLLOWING);
+        FriendshipsFeedResponse followingsResponse = client.sendRequest(followingsRequest).get();
 
-        FriendshipsFeedRequest friendshipsFeedFirstRequest = new FriendshipsFeedRequest(client.getPk(), FriendshipsFeedRequest.FriendshipsFeeds.FOLLOWING);
-        FriendshipsFeedResponse friendshipsFeedFirstResponse = client.sendRequest(friendshipsFeedFirstRequest).get();
-
-        if(friendshipsFeedFirstResponse.getStatus().equals("ok")) {
-            followersList.addAll(friendshipsFeedFirstResponse.getUsers());
-            maxId = friendshipsFeedFirstResponse.getNextMaxId();
-
-            while (maxId != null) {
-                FriendshipsFeedRequest friendshipsFeedPaginatedRequest = new FriendshipsFeedRequest(client.getPk(), FriendshipsFeedRequest.FriendshipsFeeds.FOLLOWING, maxId);
-                FriendshipsFeedResponse friendshipsFeedPaginatedResponse = client.sendRequest(friendshipsFeedPaginatedRequest).get();
-                if (friendshipsFeedPaginatedResponse.getStatus().equals("ok")) {
-                    maxId = friendshipsFeedPaginatedResponse.getNextMaxId();
-                    followersList.addAll(friendshipsFeedPaginatedResponse.getUsers());
-                }
-            }
-        }
-
-        assertNotNull(followersList);
+        assertEquals("ok", followingsResponse.getStatus());
     }
 
     @Test
     public void friendshipsFeedFollowersTest() throws Exception {
-        String maxId;
-        List<User> followersList = new ArrayList<>();
+        FriendshipsFeedRequest followersRequest = new FriendshipsFeedRequest(client.getPk(), FriendshipsFeedRequest.FriendshipsFeeds.FOLLOWERS);
+        FriendshipsFeedResponse followersResponse = client.sendRequest(followersRequest).get();
 
-        FriendshipsFeedRequest friendshipsFeedFirstRequest = new FriendshipsFeedRequest(client.getPk(), FriendshipsFeedRequest.FriendshipsFeeds.FOLLOWERS);
-        FriendshipsFeedResponse friendshipsFeedFirstResponse = client.sendRequest(friendshipsFeedFirstRequest).get();
-
-        if(friendshipsFeedFirstResponse.getStatus().equals("ok")) {
-            followersList.addAll(friendshipsFeedFirstResponse.getUsers());
-            maxId = friendshipsFeedFirstResponse.getNextMaxId();
-
-            while (maxId != null) {
-                FriendshipsFeedRequest friendshipsFeedPaginatedRequest = new FriendshipsFeedRequest(client.getPk(), FriendshipsFeedRequest.FriendshipsFeeds.FOLLOWERS, maxId);
-                FriendshipsFeedResponse friendshipsFeedPaginatedResponse = client.sendRequest(friendshipsFeedPaginatedRequest).get();
-                if (friendshipsFeedPaginatedResponse.getStatus().equals("ok")) {
-                    maxId = friendshipsFeedPaginatedResponse.getNextMaxId();
-                    followersList.addAll(friendshipsFeedPaginatedResponse.getUsers());
-                }
-            }
-        }
-
-        assertNotNull(followersList);
+        assertEquals("ok", followersResponse.getStatus());
     }
 }
