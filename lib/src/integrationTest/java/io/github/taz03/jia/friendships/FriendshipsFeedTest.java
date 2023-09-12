1a09c7d9d0338251818bdcb062d5fff0caa33055
Tanish Azad <tanishazad03@gmail.com>
2023-09-12 07:43:46 +0530

    used instagram pk in friendships feed test

--- lib/src/integrationTest/java/io/github/taz03/jia/friendships/FriendshipsFeedTest.java
+++ lib/src/integrationTest/java/io/github/taz03/jia/friendships/FriendshipsFeedTest.java
@@ -4,14 +4,18 @@ import static io.github.taz03.jia.TestProxy.client;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import io.github.taz03.jia.requests.friendships.FriendshipsFeedRequest;
+import io.github.taz03.jia.requests.friendships.FriendshipsFeedRequest.FriendshipsFeeds;
 import io.github.taz03.jia.responses.friendships.FriendshipsFeedResponse;
 
 import org.junit.jupiter.api.Test;
 
 public final class FriendshipsFeedTest {
+    // pk of official instagram account - https://www.instagram.com/instagram/
+    private static final long INSTAGRAM_PK = 25025320;
+
     @Test
     public void friendshipsFeedFollowingTest() throws Exception {
-        FriendshipsFeedRequest followingsRequest = new FriendshipsFeedRequest(client.getPk(), FriendshipsFeedRequest.FriendshipsFeeds.FOLLOWING);
+        FriendshipsFeedRequest followingsRequest = new FriendshipsFeedRequest(INSTAGRAM_PK, FriendshipsFeeds.FOLLOWING);
         FriendshipsFeedResponse followingsResponse = client.sendRequest(followingsRequest).get();
 
         assertEquals("ok", followingsResponse.getStatus());
@@ -19,7 +23,7 @@ public final class FriendshipsFeedTest {
 
     @Test
     public void friendshipsFeedFollowersTest() throws Exception {
-        FriendshipsFeedRequest followersRequest = new FriendshipsFeedRequest(client.getPk(), FriendshipsFeedRequest.FriendshipsFeeds.FOLLOWERS);
+        FriendshipsFeedRequest followersRequest = new FriendshipsFeedRequest(INSTAGRAM_PK, FriendshipsFeeds.FOLLOWERS);
         FriendshipsFeedResponse followersResponse = client.sendRequest(followersRequest).get();
 
         assertEquals("ok", followersResponse.getStatus());
