c486faeb29b072bb254548248ef32701ee94c9ac
Tanish <tanishazad03@gmail.com>
2023-09-12 07:30:00 +0530

    Friendships show request (#40)

---
+++ lib/src/main/java/io/github/taz03/jia/requests/friendships/ShowRequest.java
@@ -0,0 +1,18 @@
+package io.github.taz03.jia.requests.friendships;
+
+import io.github.taz03.jia.requests.InstagramGetRequest;
+import io.github.taz03.jia.responses.friendships.ShowResponse;
+
+/**
+ * Represents an Instagram request, used to show your connection info with others.
+ */
+public final class ShowRequest extends InstagramGetRequest<ShowResponse> {
+    /**
+     * Creates an Instagram show request.
+     *
+     * @param pk pk of the user to show connection info for
+     */
+    public ShowRequest(long pk) {
+        super(ShowResponse.class, "/api/v1/friendships/show/%d/".formatted(pk));
+    }
+}
