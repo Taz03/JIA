bf9b6be4a14081a8f304154e959a30cd27656412
Tanish <tanishazad03@gmail.com>
2023-11-09 01:11:31 +0530

    fixed docs (#56)

--- lib/src/main/java/io/github/taz03/jia/requests/accounts/RemoveProfilePictureRequest.java
+++ lib/src/main/java/io/github/taz03/jia/requests/accounts/RemoveProfilePictureRequest.java
@@ -7,6 +7,9 @@ import io.github.taz03.jia.responses.users.UserResponse;
  * Represents an Instagram remove profile picture request, used to remove profile picture.
  */
 public final class RemoveProfilePictureRequest extends InstagramPostRequest<UserResponse> {
+	/**
+	 * Create an Instagram remove profile picture request.
+	 */
 	public RemoveProfilePictureRequest() {
 		super(UserResponse.class, "/api/v1/accounts/remove_profile_picture/");
 	}
